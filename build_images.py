5bb6df0f3f457d5e0cf5d321b7ea3ac384f5663b
Ben Du <longendu@yahoo.com>
2026-04-18 09:33:12 -0700

    update base image tag before building

--- build_images.py
+++ build_images.py
@@ -7,6 +7,7 @@
 # ///
 
 import datetime
+import re
 import subprocess as sp
 import sys
 from dulwich.repo import Repo
@@ -37,7 +38,58 @@ DIRS = [
 ]
 
 
-def tag_date(tag: str) -> str:
+def _update_base_image_tag_text(text: str) -> str:
+    text = re.sub(r"(\nFROM dclong/\w+)\n", r"\1:next\n", text)
+    text = re.sub(r"(\nCOPY --from=dclong/\w+(-\w+)?) ", r"\1:next ", text)
+    return text
+
+
+def test_update_base_image_tag_text():
+    text = """
+# NAME: dclong/rust-cicd
+FROM dclong/python
+
+RUN pip3 install github-rest-api
+
+ENV RUSTUP_HOME=/usr/local/rustup PATH=/usr/local/cargo/bin:$PATH
+COPY --from=dclong/rust /usr/local/rustup/ /usr/local/rustup/
+COPY --from=dclong/rust \
+        /usr/local/cargo/bin/rustc \
+        /usr/local/cargo/bin/rustdoc \
+        /usr/local/cargo/bin/cargo \
+        /usr/local/cargo/bin/cargo-fmt \
+        /usr/local/cargo/bin/rustfmt \
+    /usr/local/cargo/bin/
+COPY --from=dclong/rust-utils /usr/local/cargo/bin/cargo-criterion /usr/local/cargo/bin/
+COPY --from=dclong/rust-utils /usr/bin/nperf /usr/bin/
+COPY settings/sysctl.conf /etc/sysctl.conf
+""".strip()
+    text_truth = (
+        text.replace(
+            "FROM dclong/python",
+            "FROM dclong/python:next",
+        )
+        .replace(
+            "--from=dclong/rust-utils ",
+            "--from=dclong/rust-utils:next ",
+        )
+        .replace(
+            "--from=dclong/rust ",
+            "--from=dclong/rust:next ",
+        )
+    )
+    assert _update_base_image_tag_text(text) == text_truth
+
+
+def _update_base_image_tag(dir_: str, tags: list[str]) -> None:
+    if "latest" in tags:
+        return
+    dockerfile = dir_ / "Dockerfile"
+    text = dockerfile.read_text()
+    dockerfile.write_text(_update_base_image_tag_text(text))
+
+
+def _tag_date(tag: str) -> str:
     """Suffix a tag with the current date as a 6-digit string.
 
     :param tag: A tag of Docker image.
@@ -49,7 +101,7 @@ def tag_date(tag: str) -> str:
 @retry(
     stop=stop_after_attempt(3), wait=wait_exponential(multiplier=60, min=60, max=300)
 )
-def push_image(image: str):
+def _push_image(image: str):
     sp.run(
         ["docker", "push", image],
         shell=False,
@@ -60,6 +112,7 @@ def push_image(image: str):
 def _build_image(dir_: str, tags: str | list[str]):
     if isinstance(tags, str):
         tags = [tags]
+    _update_base_image_tag(dir_, tags)
     image = dir_.replace("docker-", "dclong/")
     print(f"\n\nBuilding the Docker image {image}...", flush=True)
     cmd = ["docker", "build", dir_]
@@ -68,10 +121,10 @@ def _build_image(dir_: str, tags: str | list[str]):
         cmd.append(f"{image}:{tag}")
     sp.run(cmd, shell=False, check=True)
     for tag in tags:
-        push_image(f"{image}:{tag}")
+        _push_image(f"{image}:{tag}")
 
 
-def is_no_diff() -> bool:
+def _is_no_diff() -> bool:
     """Check whether there is no difference between dev/main branches."""
     repo = Repo(".")
     url = repo.get_config().get(("remote", "origin"), "url").decode("utf-8")
@@ -85,9 +138,9 @@ def is_no_diff() -> bool:
 
 def build_images():
     tags = ["next"]
-    if is_no_diff():
+    if _is_no_diff():
         tags.append("latest")
-    tags.extend([tag_date(tag) for tag in tags])
+    tags.extend([_tag_date(tag) for tag in tags])
     print("Building Docker images using tags:", ", ".join(tags), "\n", flush=True)
     failures = []
     for dir_ in DIRS:
