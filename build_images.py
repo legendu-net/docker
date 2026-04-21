88c806c40617773e7d2034c07b0d6c5dc9569e36
Ben Du <longendu@yahoo.com>
2026-04-21 00:24:27 -0700

    simplify build_images.py

--- build_images.py
+++ build_images.py
@@ -7,8 +7,6 @@
 # ///
 
 import datetime
-from pathlib import Path
-import re
 import subprocess as sp
 import sys
 from dulwich.repo import Repo
@@ -39,58 +37,6 @@ DIRS = [
 ]
 
 
-def _update_base_image_tag_text(text: str) -> str:
-    pattern_image = r"dclong/\w+(-\w+)?"
-    text = re.sub(rf"(\nFROM {pattern_image})\n", r"\1:next\n", text)
-    text = re.sub(rf"(\nCOPY --from={pattern_image}) ", r"\1:next ", text)
-    return text
-
-
-def test_update_base_image_tag_text():
-    text = """
-# NAME: dclong/rust-cicd
-FROM dclong/python
-
-RUN pip3 install github-rest-api
-
-ENV RUSTUP_HOME=/usr/local/rustup PATH=/usr/local/cargo/bin:$PATH
-COPY --from=dclong/rust /usr/local/rustup/ /usr/local/rustup/
-COPY --from=dclong/rust \
-        /usr/local/cargo/bin/rustc \
-        /usr/local/cargo/bin/rustdoc \
-        /usr/local/cargo/bin/cargo \
-        /usr/local/cargo/bin/cargo-fmt \
-        /usr/local/cargo/bin/rustfmt \
-    /usr/local/cargo/bin/
-COPY --from=dclong/rust-utils /usr/local/cargo/bin/cargo-criterion /usr/local/cargo/bin/
-COPY --from=dclong/rust-utils /usr/bin/nperf /usr/bin/
-COPY settings/sysctl.conf /etc/sysctl.conf
-""".strip()
-    text_truth = (
-        text.replace(
-            "FROM dclong/python",
-            "FROM dclong/python:next",
-        )
-        .replace(
-            "--from=dclong/rust-utils ",
-            "--from=dclong/rust-utils:next ",
-        )
-        .replace(
-            "--from=dclong/rust ",
-            "--from=dclong/rust:next ",
-        )
-    )
-    assert _update_base_image_tag_text(text) == text_truth
-
-
-def _update_base_image_tag(dir_: str, tags: list[str]) -> None:
-    if "latest" in tags:
-        return
-    dockerfile = Path(dir_) / "Dockerfile"
-    text = dockerfile.read_text()
-    dockerfile.write_text(_update_base_image_tag_text(text))
-
-
 def _tag_date(tag: str) -> str:
     """Suffix a tag with the current date as a 6-digit string.
 
@@ -114,7 +60,6 @@ def _push_image(image: str):
 def _build_image(dir_: str, tags: str | list[str]):
     if isinstance(tags, str):
         tags = [tags]
-    _update_base_image_tag(dir_, tags)
     image = dir_.replace("docker-", "dclong/")
     print(f"\n\nBuilding the Docker image {image}...", flush=True)
     cmd = ["docker", "build", dir_]
