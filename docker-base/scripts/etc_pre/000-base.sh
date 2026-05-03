29de619e69ac1a0b8deeae2a60056bbe7c324fc5
Ben Du <longendu@yahoo.com>
2026-05-03 08:58:04 -0700

    move neovim from docker-base to docker-jupyterlab and install via brew

--- docker-base/scripts/etc_pre/000-base.sh
+++ docker-base/scripts/etc_pre/000-base.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 su -m $DOCKER_USER -c "icon git -c"
-su -m $DOCKER_USER -c "icon nvim -c"
 
--- docker-jupyterlab/scripts/etc_pre/030-jupyterlab.sh
+++ docker-jupyterlab/scripts/etc_pre/030-jupyterlab.sh
@@ -16,4 +16,5 @@ _copy_config settings.jupyterlab-settings jupyterlab_code_formatter
 chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.jupyter
 
 su -m $DOCKER_USER -c "icon fish -c"
+su -m $DOCKER_USER -c "icon nvim -c"
 
